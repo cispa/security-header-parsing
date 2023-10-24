2b2b28b4a66f81ad1b4ab787e8b73cb0a2c378e5
hp23 Server <hp23@01.saarland>
2023-10-24 08:42:17 +0000

    Add other CORP tests

--- _hp/server/responses.py
+++ _hp/server/responses.py
@@ -16,19 +16,22 @@ def get_response(resp_id):
         return response
     
 @lru_cache(maxsize=None)
-def get_body(feature_group):
+def get_body(feature_group, nest):
     print("Body:", feature_group, os.getpid(), threading.current_thread().ident)
     # Default: iframes.html
-    file = open("_hp/common/iframes.html","rb")
+    file = open("_hp/common/iframes.html", "rb")
     # Other body for other tests
     if feature_group in ['accessapi']:
-        file = open(f"_hp/common/iframe-api.html","rb")
+        file = open(f"_hp/common/iframe-api.html", "rb")
     if feature_group in ['rp','pp','coop']:
-        file = open(f"_hp/common/{feature_group}.html","rb")
+        file = open(f"_hp/common/{feature_group}.html", "rb")
     elif feature_group in ['coep','oac']:
-        file = open(f"_hp/common/{feature_group}test.html","rb")
+        file = open(f"_hp/common/{feature_group}test.html", "rb")
     elif feature_group=='corp':
-        file = open("_hp/common/swag.jpg","rb")
+        if nest == 0:
+            file = open("_hp/common/swag.jpg", "rb")
+        else:
+            file = open("_hp/common/frame-corp.html", "rb")
     return file.read()
 
 #@handler
@@ -36,11 +39,12 @@ def main(request, response):
     params = request.GET
     # print(params)
     # Get the correct response based on resp_id (headers + status code, without body)
-    if int(params["nest"]) == 0:
+    nest = int(params["nest"]) 
+    if nest == 0:
         response = get_response(params["resp_id"])
     else:
         response.status_code = 200
         response.raw_header = []
     # Get the correct response body based on the current test/feature group
-    file = get_body(params['feature_group'])
+    file = get_body(params['feature_group'], nest=nest)
     return response.status_code, response.raw_header, file
