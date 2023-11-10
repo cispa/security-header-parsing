e7c932b4ca49aa546b0ab7445fb68f59e9dd6204
JannisBush <33023300+JannisBush@users.noreply.github.com>
2023-11-10 14:43:21 +0100

    MAC selenium runners

--- _hp/tools/crawler/browser_mac_selenium.py
+++ _hp/tools/crawler/browser_mac_selenium.py
@@ -5,55 +5,60 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def get_browser(browser: str, version: str, binary_location=None, arguments=None):
+    if browser == "chrome":
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome
+    elif browser == "firefox":
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox
+    elif browser == "safari":
+        options = webdriver.SafariOptions()
+        driver = webdriver.Safari
+    elif browser == "edge":
+        options = webdriver.EdgeOptions()
+        driver = webdriver.Edge
+    else:
+        print(f"Unsupported browser: {browser}")
+        return Exception()
+    
+    options.browser_version = version
+    if binary_location:
+        options.binary_location = binary_location # Possible to specify other browsers? e.g., brave?
+    if arguments:
+        options.add_argument(arguments) # ("--headless=new") # Possible to add arguments such as headless?
+    return driver(options=options)
 
 
-def main():
-    # TODO use correct browser_id, basic mode, both schemes, ...
-    test_urls = get_tests(resp_type="debug", browser_id=1, scheme="https")
-    driver = webdriver.Safari()
-    try:
-        for url in test_urls:
-            driver.get(url)
-            print(driver.title)
-            # Wait until the results are saved on the server (after finishing fetch request, a div with id "finished" is added to the DOM)
-            WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.ID, "finished")))
-    except Exception as e:
-        print(e)
-    finally:
-        driver.quit()
+def main(browser_name, browser_version, binary_location, arguments, browser_id):
+    for scheme in ["http", "https"]:
+        test_urls = get_tests(resp_type=MODE, browser_id=browser_id, scheme=scheme)
+        driver = get_browser(browser_name, browser_version, binary_location, arguments)
+        print(driver.capabilities)
+        try:
+            1/0
+            for url in test_urls:
+                driver.get(url)
+                print(driver.title)
+                # Wait until the results are saved on the server (after finishing fetch request, a div with id "finished" is added to the DOM)
+                WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.ID, "finished")))
+        except Exception as e:
+            print("Exception occured!")
+            print(e)
+        finally:
+            driver.quit()
 
-
+MODE = "basic"  # "debug", "parsing"
 if __name__ == '__main__':
-    main()
-
-    # TODO: define and create different browsers
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.browser_version = "118"
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://example.org")
-    time.sleep(10)
-    driver.quit()
-
-    options = webdriver.FirefoxOptions()
-    options.browser_version = "115" #119
-    driver = webdriver.Firefox(options=options)
-    driver.get("https://example.org")
-    time.sleep(10)
-    driver.quit()
-
-    options = webdriver.SafariOptions()
-    options.browser_version = "17.0"
-    driver = webdriver.Safari(options=options)
-    driver.get("https://example.org")
-    time.sleep(10)
-    driver.quit()
-
-    options = webdriver.EdgeOptions()
-    options.browser_version = "119"
-    driver = webdriver.Edge(options=options)
-    driver.get("https://example.org")
-    time.sleep(10)
-    driver.quit()
+    # (browser_name, version, binary_location (e.g., for brave), arguments (e.g, for headless), browser_id)
+    config = [
+        ("chrome", "119", None, None, 2),
+        ("firefox", "119", None, None, 3),
+        ("safari", "17.0", None, None, 4),
+        ("edge", "119", None, None, 5)
+    ]
+    for t in config:
+        main(*t)
 
     # Alternative idea with grid
     # java -jar selenium-server-4.15.0.jar standalone --selenium-manager True
--- _hp/tools/create_browsers.py
+++ _hp/tools/create_browsers.py
@@ -1,9 +1,23 @@
 # TODO: fill the Browser table with all browsers used!
+from models import Session, Browser
+from sqlalchemy.exc import IntegrityError
 
-
+def create_browser(name, version, os, headless_mode, automation_mode, add_info):
+    with Session() as session:
+        try:
+            r = Browser(name=name, version=version, os=os, headless_mode=headless_mode, automation_mode=automation_mode, add_info=add_info)
+            session.add(r)
+            session.commit()
+        except IntegrityError as e:
+            session.rollback()
+            print("IntegrityError probably response already exists")
 
 # MAC (local) browsers with Selenium automation
-#TODO
+os = "macOS 14.0"
+automation_mode = "selenium"
+headless_mode = "real"
+for (name, version) in [("chrome", "119"), ("firefox", "119"), ("safari", "17.0"), ("edge", "119")]:
+    create_browser(name, version, os, headless_mode, automation_mode, "")
 
 # Linux browsers managed by Selenium
 #TODO
