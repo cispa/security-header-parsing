9058da0666350d8ed225a8c204eb81fac09bb12d
Tin Nguyen <tin.nguyen@cispa.saarland>
2023-11-15 12:28:55 +0100

    Working with multiple browsers

--- _hp/tools/crawler/android/android_intent.py
+++ _hp/tools/crawler/android/android_intent.py
@@ -25,52 +25,44 @@ class PARAMETER:
 		self.app_list = app_list
 
 class APP:
-	def __init__(self, package_name, activity_name, apk_file, test_url):
+	def __init__(self, package_name, activity_name, test_url):
 		self.package_name = package_name
 		self.activity_name = activity_name
-		self.device_id = None	
-		self.apk_file = apk_file
+		self.device_id = None			
 		self.test_url = test_url
 	
 	def set_test_device(self, device_id):
 		self.device_id = device_id
 
 def run_test(parameter):
-	browser_factor = 1	
-
 	for app in parameter.app_list:		
-		print(f'Testing: {app.package_name}, on device ID: {app.device_id}, with URL: {app.test_url}')		
-		emulators.install_app(app.package_name, app.apk_file, app.device_id)		
-		if app.package_name == 'com.UCMobile.intl':
-			browser_factor = 2
-
-		time.sleep(5*browser_factor)
-		
+		print(f'Testing: {app.package_name}, on device ID: {app.device_id}, with URL: {app.test_url}')				
+		return
 		# starting the browser to create init profile
 		emulators.send_url_intent(app.device_id, app.package_name, app.activity_name, 'www.google.com')
-		time.sleep(2*browser_factor)
+		time.sleep(2)
 		emulators.enable_popup(app.device_id, app.package_name)
 		
 		encoded_test_url = app.test_url.replace('&','\&')
 		
 		emulators.send_url_intent(app.device_id, app.package_name, app.activity_name, encoded_test_url)
-		time.sleep(TIMEOUT*1000)
+		time.sleep(TIMEOUT*1000)		
 		
-		emulators.uninstall_app(app.package_name, app.device_id)
-		time.sleep(5*browser_factor)
+		time.sleep(5)
 
-def main(browser_config):
-	browser_id = browser_config['id']
-	test_urls = list()
-	for scheme in ["http", "https"]:
-		test_urls.extend(get_tests(resp_type=MODE, browser_id=browser_id, scheme=scheme))
-	
-	device_ids = emulators.get_available_device()
-	apk_file = os.path.join(APK_DIR, browser_config['apk_file'])
-	
+def main(browser_list, config_dict):
 	app_list = list()
-	for url in test_urls:
-		app_list.append(APP(browser_config['package_name'], browser_config['intent'], apk_file, url))
+	for browser_name in browser_list:
+		browser_config = config_dict[browser_name]
+
+		browser_id = browser_config['id']
+		test_urls = list()
+		for scheme in ["http", "https"]:
+			test_urls.extend(get_tests(resp_type=MODE, browser_id=browser_id, scheme=scheme))
+	
+		device_ids = emulators.get_available_device()	
+		for url in test_urls:
+			app_list.append(APP(browser_config['package_name'], browser_config['intent'], url))
 
 	chunked_app_lists = np.array_split(app_list, len(device_ids))
 	
@@ -89,9 +81,14 @@ def main(browser_config):
 if __name__ == '__main__':
 	with open('android_config.json') as file:
 		config_dict = json.load(file)
-	
+
+	browser_list = list(config_dict.keys())
+	browser_list.append('all')
+
 	ap = argparse.ArgumentParser(description='Tester for Android devices')
-	ap.add_argument('-browser', '--browser', dest='browser', type=str, required=True, choices=config_dict.keys())
+	ap.add_argument('-browsers', '--browsers', dest='browsers', type=str, required=True, nargs='+', choices=browser_list)
 	args = ap.parse_args()
+
+	print(f'Starting test on {args.browsers} ...')	
 	
-	main(config_dict[args.browser])
+	main(args.browsers, config_dict)
