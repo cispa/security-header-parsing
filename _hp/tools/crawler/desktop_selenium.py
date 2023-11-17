37cc3c36bbdb3a647ba01551cf6353c85eaf0de3
hp23 Server <hp23@01.saarland>
2023-11-17 18:01:16 +0000

    Minor logging improvements

--- _hp/tools/crawler/desktop_selenium.py
+++ _hp/tools/crawler/desktop_selenium.py
@@ -6,10 +6,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 import traceback
+import datetime
+
 
 class Tee(object):
     def __init__(self, filename, name):
-        self.file = open(f"{filename}-{name}", 'a')
+        self.file = open(f"{filename}-{name}.log", 'a')
         self.stdout = sys.stdout
         self.name = name
 
@@ -25,7 +27,7 @@ class Tee(object):
 
     def write(self, data):
         if data != "\n" and data != " " and data != "":
-            data = f"{self.name}: {data}"
+            data = f"{datetime.datetime.now()}-{self.name}: {data}"
         self.file.write(data)
         self.stdout.write(data)
 
@@ -40,7 +42,7 @@ def get_browser(browser: str, version: str, binary_location=None, arguments=None
         options = webdriver.ChromeOptions()
         driver = webdriver.Chrome
         # Optional use different ChromeDriver version!, default is chosen from the given browser_version
-        #if browser == "brave":
+        # if browser == "brave":
         #    service = Service("/Applications/chromedriver")
     elif browser == "firefox":
         options = webdriver.FirefoxOptions()
@@ -54,23 +56,28 @@ def get_browser(browser: str, version: str, binary_location=None, arguments=None
     else:
         print(f"Unsupported browser: {browser}")
         return Exception()
-    
+
     options.browser_version = version
     if binary_location:
-        options.binary_location = binary_location # Possible to specify other browsers e.g., brave
+        # Possible to specify other browsers e.g., brave
+        options.binary_location = binary_location
     if arguments:
         for argument in arguments:
-            options.add_argument(argument) # ("--headless=new") # Possible to add arguments such as headless
+            # ("--headless=new") # Possible to add arguments such as headless
+            options.add_argument(argument)
     print(options.to_capabilities())
     return driver(options=options, service=service)
 
 
 def main(browser_name, browser_version, binary_location, arguments, browser_id):
     for scheme in ["http", "https"]:
-        test_urls = get_tests(resp_type=MODE, browser_id=browser_id, scheme=scheme)
-        driver = get_browser(browser_name, browser_version, binary_location, arguments)
+        test_urls = get_tests(
+            resp_type=MODE, browser_id=browser_id, scheme=scheme)
+        driver = get_browser(browser_name, browser_version,
+                             binary_location, arguments)
         # Max page load timeout
         driver.set_page_load_timeout(TIMEOUT*2)
+        print(f"Start {browser_name} ({browser_version}) ({scheme})")
         print(driver.capabilities)
         # Store the ID of the original window
         original_window = driver.current_window_handle
@@ -82,12 +89,14 @@ def main(browser_name, browser_version, binary_location, arguments, browser_id):
                     if "upgrade" in url:
                         driver.get(HSTS_DEACTIVATE)
                     driver.get(url)
-                    print(driver.title)
+                    # print(driver.title)
                     # Wait until the results are saved on the server (after finishing fetch request, a div with id "finished" is added to the DOM)
-                    WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.ID, "finished")))
+                    WebDriverWait(driver, TIMEOUT).until(
+                        EC.presence_of_element_located((By.ID, "finished")))
                 except Exception as e:
                     print("Exception!", e)
                     print(driver.current_url)
+                    print(url)
                 finally:
                     # input("Next")  # Option to manualy debug
                     # Close the current window
@@ -95,23 +104,25 @@ def main(browser_name, browser_version, binary_location, arguments, browser_id):
                     # Switch back to the old tab or window
                     driver.switch_to.window(original_window)
         except Exception as e:
-            print("Exception occured!")
-            print(e)
+            print("Major Exception occured!", e)
         finally:
             driver.quit()
+            print(f"Finish {browser_name} ({browser_version}) ({scheme})")
+
 
 MODE = "basic"  # "debug", "parsing"
 if __name__ == '__main__':
     # (browser_name, version, binary_location (e.g., for brave), arguments (e.g, for headless), browser_id)
     if sys.platform == "darwin":
         config = [
-            #("chrome", "119", None, None, 5),
-            #("firefox", "119", None, None, 6),
-            #("safari", "17.0", None, None, 7),
-            #("edge", "119", None, None, 8),
+            # ("chrome", "119", None, None, 5),
+            # ("firefox", "119", None, None, 6),
+            # ("safari", "17.0", None, None, 7),
+            # ("edge", "119", None, None, 8),
             # Download .dmg from https://github.com/brave/brave-browser/releases and install
             # E.g., https://github.com/brave/brave-browser/releases/tag/v1.60.118, rename the file
-            ("brave", "119", "/Applications/Brave Browser 1.60.118.app/Contents/MacOS/Brave Browser", ["--disable-brave-update"], 37),
+            ("brave", "119", "/Applications/Brave Browser 1.60.118.app/Contents/MacOS/Brave Browser",
+             ["--disable-brave-update"], 37),
         ]
     # Linux Ubuntu
     else:
@@ -128,9 +139,11 @@ if __name__ == '__main__':
             # The ZIP versions seem to not auto update and one can install as many as wanted (only on linux though?)
             # v1.59.120 (Chromium 118): wget https://github.com/brave/brave-browser/releases/download/v1.59.120/brave-browser-1.59.120-linux-amd64.zip
             # Note: if you specify the wrong chromium version, selenium will ignore the binary location and download CFT instead??
-            ("brave", "118", "/home/ubuntu/brave-versions/v1.59.120/brave-browser", ["--headless=new"], 60),
+            ("brave", "118", "/home/ubuntu/brave-versions/v1.59.120/brave-browser",
+             ["--headless=new"], 60),
             # v1.60.114 (Chromium 119): wget https://github.com/brave/brave-browser/releases/download/v1.60.114/brave-browser-1.60.114-linux-amd64.zip
-            ("brave", "119", "/home/ubuntu/brave-versions/v1.60.114/brave-browser", ["--headless=new"], 59),
+            ("brave", "119", "/home/ubuntu/brave-versions/v1.60.114/brave-browser",
+             ["--headless=new"], 59),
             # Headfull
             ("chrome", "119", None, None, 71),
             ("firefox", "119", None, None, 72),
