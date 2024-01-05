e1b1e5b425f2b0c38c7f31701accf86a1b5de067
Trung Tin Nguyen <tin.nguyen@cispa.de>
2024-01-05 13:00:08 +0100

    Update create_browsers.py

--- _hp/tools/create_browsers.py
+++ _hp/tools/create_browsers.py
@@ -1,14 +1,13 @@
 from crawler.utils import get_or_create_browser
 
 # Android browsers
-os = "Android 11"
-automation_mode = "intent"
-headless_mode = "real"
-for (name, version) in [("firefox", "119.1.1"), ("brave", "1.60.116"), ("ucmobile", "13.3.8.1305"), ("opera", "78.4.4143.75735")]:
-    get_or_create_browser(name, version, os, headless_mode, automation_mode, "")
+# Will be created automatically based on the android_config.json file
 
 # iPhone browsers
-#TODO
+ios_browser_list = [('chrome', '120.0.6099.119'), ('brave', '1.61 (23.12.18.22)'), ('opera', '4.4.1 (0)'), ('firefox', '121.0 (36782)'), ('safari', '17'), ('operagx', '2.2.2'), ('edge', '120.0.2210.116')]
+
+for browser_name, browser_version in ios_browser_list:		
+    browser_id = get_or_create_browser(browser_name, browser_version, 'iPadOS 17.2', 'real', 'intent', '')	
 
 # Mac browsers managed by PLaywright
 os = "macOS 14.0"
