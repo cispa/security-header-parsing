095c1a38655d7f424bba503612be52586059288f
hp23 Server <hp23@01.saarland>
2024-01-24 12:01:46 +0000

    Cleanup

--- _hp/tools/create_responses.py
+++ _hp/tools/create_responses.py
@@ -17,9 +17,8 @@ parent_childs = "*.headers.websec.saarland"
 self_childs = "*.sub.headers.websec.saarland"
 
 
-# TODO: create responses for each feature group (file in /tests)
 # Some feature groups have more than one group of responses (label in the DB), e.g., framing has both XFO, CSP, and XFO vs CSP
-# We have to define somewhere which label responses we use for which test_file tests?
+# crawler/utils.py defines which labels belong to which test file
 
 def create_responses(header_list, label, status_code=200, resp_type="debug"):
     with Session() as session:
@@ -54,42 +53,6 @@ create_responses(header_list, label, resp_type="basic")
 header_list = [[(header_name, v2), redirect_empty], [(header_name, v3), redirect_empty]]
 create_responses(header_list, label, status_code=302, resp_type="basic")
 
-# WPT tests: https://wpt.fyi/results/x-frame-options?label=master&label=experimental&aligned&q=x-frame
-# Basic tests: same-origin, cross-origin
-# Special tests: XFO on redirect, nested same/cross-origin
-# WPT headers:
-# DENY, denY, <SPACE>DENY<SPACE>, sameOrigin, SAMEORIGIN, ...
-# empty values and commas
-# invalid: allow-from, allow-from=, ALLOWALL, "<value(s)>", empty string
-wpt_values = {'', 'allowAll', 'ALLOWALL', 'DENY', 'sameOriGin', 'ALLOW-FROM=https://example.com/', '"DENY"', '"SAMEORIGIN"', 'INVALID', 'SAMEORIGIN', '  SAMEORIGIN,    DENY', 'sameOrigin', '  DENY ', '"SAMEORIGIN,DENY"', '  SAMEORIGIN ', 'ALLOW-FROM https://example.com/', 'denY', ',SAMEORIGIN,,DENY,'}
-# multiple with , and multiple headers (marked with ; in their tests)
-# XFO + CSP-FA
-
-# Siewert tests:
-# Basic tests: same-origin, cross-origin (subdomain) ; no cross-site! no nested/double framing, ...!
-# Special test: XFO in meta tag (does not have any effect)
-# Siewert headers:
-siewert_values = {'allow-from http://randomorigin.com, sameorigin', 'sameorigin; sameorigin', 'RANDOMDIRECTIVE', 'allow-from http://randomorigin.com/, deny', 'sameorigin; deny', 'allow-from http://randomorigin.com/, allow-from http://randomorigin.com/', 'allow-from http://much.ninja', 'allow-from TESTURI; deny', 'allow-from http://randomorigin.com/, allow-from TESTURI', 'allow-from TESTURI, deny', 'sameorigin, allow-from http://randomorigin.com', 'allow-from TESTURI, allow-from http://randomorigin.com/', 'deny', 'sameorigin', 'deny; deny', 'allow-from http://randomorigin.com/; allow-from http://randomorigin.com/', 'allow-from TESTURI; allow-from http://randomorigin.com/', 'deny; allow-from TESTURI', 'deny; sameorigin', 'allowall', 'sameorigin, deny', 'allow-from http://random.ninja', 'allow-from http://randomorigin.com/', 'deny, deny', 'allow-from TESTURI', 'sameorigin, sameorigin', 'allow-from https://much.ninja', 'deny, allow-from TESTURI', 'deny, sameorigin'}
-
-# crawler.ninja (https://crawler.ninja/files/xfo-values.txt) 
-# Subset of (popular) invalid choices
-crawler_values = {'GOFORIT', '*', 'ALLOW-FROM *', 'same-origin', 'â€œDENYâ€', 'ALLOW-FROM SAMEDOMAIN', 'NONE', 'SAMEORIGIN always', 'ALLOW-FROM \'self\'', 'CROSS-ORIGIN', 'FALSE', 'SAMEORIGIN <url>', ': SAMEORIGIN', '<url>', '"SAMEORIGIN" always;', 'NEVER', 'SAMESITE', 'strict-origin-when-cross-origin', '(DENY || SAMEORIGIN)', 'self', 'X-Frame-Options: DENY', 'add_header X-Frame-Options "SAMEORIGIN" always;', 'nosniff', 'ALLOW-ALL', 'crossorigin', 'null', 'ANY', 'all', 'SMAEORIGIN', 'sameoriginSAMEORIGIN', "'DENY'", 'SAMEORIGINS', 'SAMEORIGION'}
-
-
-# Other values: (own ideas)
-# Firefox replaces all whitespaces, chromium only strips them
-# Also Tab %09, vertical tab %0B, (all control characters!)
-# %C2%A0 (non-breaking space) ...
-# Separation with space: sameorigin sameorigin
-# Full-Width comma? ，%EF%BC%8C
-# ...
-other_values = {"same origin", "den y"}
-
-
-# Alternative header names (legacy/invalid/typos)
-# For main name use different casing: x-frame-options, X-FRAME-OPTIONS, X-Frame-Options, X-frame-options
-# Idea for alternative header names: Test with one basic testcase (DENY) whether the wrong header name works? If not, skip all tests, if yes run all tests to see if they behave the same or not
-alt_header_names = ["Frame-Options", "X-Frame-Option", "X-FRAMES-OPTIONS", "Content-Security-Policy", "X_FRAME_OPTIONS", "XFRAMEOPTIONS"]
 
 ## CSP-FA
 label = "CSP-FA"
@@ -121,30 +84,6 @@ create_responses(header_list, label, resp_type="basic")
 header_list = [[(header_name, v2), redirect_empty], [(header_name, v3), redirect_empty]]
 create_responses(header_list, label, status_code=302, resp_type="basic")
 
-# WPT tests?: https://wpt.fyi/results/content-security-policy/frame-ancestors?label=master&label=experimental&aligned&q=frame
-# Simple framing: same-origin, cross-origin, nested (cross-cross, cross-same, same-cross, same-same)
-# Special stuff: service workers, overwrite XFO, sandboxed iframe parent, report fired for violations
-
-# WPT values?
-wpt_values = {"frame-ancestors 'none'", "frame-ancestors 'self'", "frame-ancestors *", "frame-ancestors http://www1.wpt.live:8000", "frame-ancestors https://wpt.live:443"}
-
-# Siewert tests?
-# Simple framing: same-origin, cross-origin (subdomain); XFO+CSP simple framing
-# Special test: meta tag
-# Siewert values?
-siewert_csp_fa = {'frame-ancestors http://much.ninja;', 'frame-ancestors much.ninja;', 'frame-ancestors *.ninja;', 'frame-ancestors http:;', 'frame-ancestors http://randomorigin.com;', 'frame-ancestors http://*.ninja http://randomorigin.com;', 'frame-ancestors https:;'}
-
-# Crawler.ninja values? https://crawler.ninja/files/csp-values.txt
-# Many more
-ninja_csp_fa = {"default-src 'none'", "self", "*", "frame-ancestors self", "frame-ancestors", "frame-ancestors none", "frame-src none",}
-
-# Other values?
-# Spaces, typo in frame-ancestors, ...
-# frame-ancestors 'null' (only allow null origin? e.g., sandboxed frame?)
-other = {}
-
-# Alternative names
-# X-Content-Security-Policy, X-Webkit-CSP, ...
 
 ## XFO vs CSP
 label = "CSPvsXFO"
