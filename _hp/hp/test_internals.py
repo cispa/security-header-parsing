f9a7e19f08ca27c5df36cd0865ab3fe1656d4f00
JannisBush <33023300+JannisBush@users.noreply.github.com>
2024-10-16 09:37:35 +0200

    Repo cleaning

--- _hp/hp/test_internals.py
+++ _hp/hp/test_internals.py
@@ -1,9 +1,8 @@
 import pytest
 
-#from wpt._hp.tools.crawler.utils import generate_short_uuid
-#from utils import generate_short_uuid
 from hp.tools.crawler.selenium_run_specific import config, run_specific
 from hp.tools.crawler.utils import generate_short_uuid
+
 
 def test_generate_short_uuid():
     assert len(generate_short_uuid(3)) == 3
@@ -11,7 +10,7 @@ def test_generate_short_uuid():
 
 def test_selenium_test_specific():
     url = "http://sub.headers.websec.saarland/_hp/tests/referrer-access-rp.sub.html?resp_type=basic&browser_id=1&label=RP&first_id=199&last_id=199&scheme=http&t_resp_id=199&t_element_relation=iframe_window.open&t_resp_origin=http://sub.headers.websec.saarland"
-    for (browser_name, browser_version, binary_location, arguments, _) in config:
+    for browser_name, browser_version, binary_location, arguments, _ in config:
         run_specific(url, browser_name, browser_version, binary_location, arguments)
 
     assert True
